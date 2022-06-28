18ce6bfec3765f9ad9e0065c4582fc3d19128c29
Alvin Kim <palifarec@gmail.com>
2022-06-28 00:08:58 -0700

    Updates

---
+++ precache-manifest.cc46474d2e05f5c6d06a7f094397fbe8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f9f53ac88c9fc391cce",
+    "url": "/portfolio/static/css/main.ef57a5cd.chunk.css"
+  },
+  {
+    "revision": "6f9f53ac88c9fc391cce",
+    "url": "/portfolio/static/js/main.4618e2dc.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "997d5f2b4dc3a4c98f2a",
+    "url": "/portfolio/static/js/2.342b0f36.chunk.js"
+  },
+  {
+    "revision": "b00b8966fd50ab3e344f49031d307c31",
+    "url": "/portfolio/static/media/selfie.b00b8966.png"
+  },
+  {
+    "revision": "86a9e9f4946f84e7a665fcd3b501f4d4",
+    "url": "/portfolio/static/media/cv.86a9e9f4.pdf"
+  },
+  {
+    "revision": "2084a03cf9382cc091170b455eaad40f",
+    "url": "/portfolio/static/media/selfie2.2084a03c.png"
+  },
+  {
+    "revision": "d3c9521d5e896dcc38e8465ada27569e",
+    "url": "/portfolio/static/media/project1.d3c9521d.jpg"
+  },
+  {
+    "revision": "44435134b793c4956a4b429a688bfe51",
+    "url": "/portfolio/static/media/portfolio2.44435134.jpg"
+  },
+  {
+    "revision": "c426c492d0feaa457b5a3a1f65fab2ac",
+    "url": "/portfolio/static/media/portfolio3.c426c492.jpg"
+  },
+  {
+    "revision": "aacaf8b22b0eb3c6ac3e5a7e707f0f7b",
+    "url": "/portfolio/index.html"
+  }
+];
